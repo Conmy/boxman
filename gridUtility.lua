c5c798eada8bcdba5b1769ee3454bf226949b348
Conmy <d.conmy1@gmail.com>
2013-06-13 00:42:47 +0100

    more files added

---
+++ gridUtility.lua
@@ -0,0 +1,27 @@
+
+-- Declare.
+local gridUtility = {}
+
+-- Methods go here.
+
+
+function gridUtility.getGrid(x, y)
+  local localgrid = {}
+  for i=0,x do
+    localgrid[i] = {}
+    for j=0,y do
+      localgrid[i][j] = 0 
+    end 
+  end
+  return localgrid
+end
+
+
+
+
+
+
+
+
+
+return gridUtility
